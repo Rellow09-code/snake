cd91fce8d52a6369d931232cf646901d328684ad
Rellow09-code <2769364@students.wits.ac.za>
2025-12-03 12:24:08 +0200

    changed: Main to main

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Footer from '@/components/Footer.vue';
 import Header from '@/components/Header.vue';
-import Main from '@/components/main.vue';
+import Main from '@/components/Main.vue';
 
 </script>
 
